07e388b819327f0c7fccf5c2ff33d59f33419ae9
Student User <student@workstation.lab.example.com>
2021-08-12 18:01:33 -0400

    a

--- wait_samples.sh
+++ wait_samples.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
-# The samples operator is so quick to re-create resources that you'd harly have a need for this script
   
-oc wait co/openshift-samples --for condition=Progressing \
+oc wait co/authentication --for condition=Progressing \
     --timeout 90s
 
-until [ "False" -eq "$(oc get co openshift-samples -o \
-jsonpath='{.status.conditions[?(@.type == "Progressing")].status}')" ]
+oc rollout status -n openshift-authentication deployment/oauth-openshift \
+    --timeout 90s
+
+while [ -n "$(oc get pod -n openshift-authentication -o \
+jsonpath='{.items[?(@.metadata.deletionTimestamp != "")].metadata.name}')" ]
 do
     sleep 3
 done
